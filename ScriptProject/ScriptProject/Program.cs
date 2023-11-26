9ef7f77f2207bfa3031fcb5280aa766182f0eb6b
Plamen pandev <looopooo45@gmail.com>
2023-11-26 21:46:23 +0200

    fuction can replac sriptpaths but in not done

--- ScriptProject/ScriptProject/Program.cs
+++ ScriptProject/ScriptProject/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace ScriptProject
 {
@@ -11,14 +12,25 @@ namespace ScriptProject
     {
         static void Main(string[] args)
         {
+            
+           //list ot listove s vseki folder i ptahofe v nego, taka shte vurti vseki folder po otdelno i shte resetvame dictonaryto da nyama poftorenie
             List<string> paths = new List<string>();
+            Dictionary<string,string> pathNames= new Dictionary<string,string>();// dictonary kudeto se zapazva path kato key i koi file tryabva da se pastne kato value
+            Dictionary<string,string>pathValues= new Dictionary<string,string>(); // dictorny kudeto key=imae na file i value=texta koito ima
+            string basePath = @"C:\Users\paco\Desktop\Project\scriptProject\";//uses in every path below
+
+            //chast s otkrivane na common virable v vsichki failove
+            //dictonary key-file value-vsichki variables
+            //sravnyava koi variables se povtaryat v vseki file i tozi koito se povtarya v vsichki se pravi nov za nego i se dobavya script path za nego v drugite filove
+
+
             //cadcloud
             /*
             paths.Add(@"C:\Users\paco\Desktop\Project\scriptProject\cadcloud");
             paths.Add(@"C:\Users\paco\Desktop\Project\scriptProject\cadcloud\Test");
             */
             //common
-            paths.Add(@"C:\Users\paco\Desktop\Project\scriptProject\common");
+            paths.Add(basePath+@"common");
             /*
             //gearsix
             paths.Add(@"C:\Users\paco\Desktop\Project\scriptProject\gearsix");
@@ -87,21 +99,53 @@ namespace ScriptProject
             {
                 foreach (string file in Directory.EnumerateFiles(path, "*.ps1"))
                 {
-                    using (var reader=new StreamReader(file))
+                    using (var reader = new StreamReader(file))
                     {
                         string lineRead;
                         while ((lineRead = reader.ReadLine()) != null)
-                        {                    
-                            Console.WriteLine( lineRead);
-                            if (lineRead.Contains(". \"$scriptPath\\")==true)
+                        {
+                            //Console.WriteLine( lineRead);
+                            if (lineRead.Contains(". \"$scriptPath\\") == true)
                             {
                                 Console.WriteLine(file);
-                                // dictonary kudeto se zapazva path kato key i koi file tryabva da se pastne kato value
+                                string search = lineRead;
+                                string code = search.Substring(15);
+                                code = code.Trim('"');
+                                Console.WriteLine(code);
+                                try
+                                {
+                                    pathNames.Add(file, code);
+                                }
+                                catch { }
+
                             }
                         }
                     }
+                    try
+                    {
+                        pathValues.Add(file.Substring(path.Length+1), File.ReadAllText(file));
+                    }catch { }                   
                 }
             }
+            //pathName key=path na fail ,value=koi file tryabva da se dobavi
+            //pathValues key=ime na fail , value=content na file
+
+            foreach (KeyValuePair<string, string> item in pathNames)
+            {
+                string text = File.ReadAllText(item.Key);
+                text = text.Replace(". \"$scriptPath\\" + item.Value, pathValues[item.Value]);
+                string path = System.IO.Path.Combine(@"C:\Users\paco\Desktop\", item.Key.Substring(item.Key.LastIndexOf('\\')+1));
+                File.WriteAllText(path, text);
+            }             
+            /*
+            foreach (var path in pathNames)
+            {
+                Console.WriteLine(path);
+            }
+            foreach (var path in pathValues)
+            {
+                Console.WriteLine(path.Key);
+            }*/
         }
     }
 }
