0a213d104d22bd759d307da092933aa06fedc185
Plamen pandev <looopooo45@gmail.com>
2023-12-06 20:50:58 +0200

    .

--- ScriptProject/ScriptProject/Program.cs
+++ ScriptProject/ScriptProject/Program.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using static System.Net.Mime.MediaTypeNames;
 
@@ -12,7 +11,8 @@ namespace ScriptProject
 {
     internal class Program
     {
-        static void Main(string[] args)
+        //method to add all paths in folders
+        public static Dictionary<string, List<string>> Init(string basePath)
         {
 
             Dictionary<string, List<string>> folderContent = new Dictionary<string, List<string>>();
@@ -27,40 +27,45 @@ namespace ScriptProject
             folderContent.Add(basePath + @"common", common);
 
             List<string> gearsix = new List<string>();
-            gearsix.Add(basePath + @"gearsix");
             gearsix.Add(basePath + @"gearsix\Life");
             gearsix.Add(basePath + @"gearsix\Test");
+            gearsix.Add(basePath + @"gearsix");
             folderContent.Add(basePath + @"gearsix", gearsix);
 
             List<string> loggedin = new List<string>();
-            loggedin.Add(basePath + @"loggedin");
             loggedin.Add(basePath + @"loggedin\Life");
             loggedin.Add(basePath + @"loggedin\Life\LifeFromPreLife");
             loggedin.Add(basePath + @"loggedin\Life\LifeFromTrunk");
             loggedin.Add(basePath + @"loggedin\PreLife");
             loggedin.Add(basePath + @"loggedin\Test");
+            loggedin.Add(basePath + @"loggedin");
             folderContent.Add(basePath + @"loggedin", loggedin);
 
+            List<string> Office = new List<string>();
+            Office.Add(basePath + @"Office.USD\Life");
+            Office.Add(basePath + @"Office.USD\Test");
+            Office.Add(basePath + @"Office.USD");
+            folderContent.Add(basePath + @"Office.USD", Office);
+
             List<string> reefr = new List<string>();
-            reefr.Add(basePath + @"reefr");
             reefr.Add(basePath + @"reefr\Life");
             reefr.Add(basePath + @"reefr\Life\LifeFromPreLife");
             reefr.Add(basePath + @"reefr\Life\LifeFromTrunk");
             reefr.Add(basePath + @"reefr\PreLife");
             reefr.Add(basePath + @"reefr\Test");
+            reefr.Add(basePath + @"reefr");
             folderContent.Add(basePath + @"reefr", reefr);
 
             List<string> safeco = new List<string>();
-            safeco.Add(basePath + @"safeco");
             safeco.Add(basePath + @"safeco\Life");
             safeco.Add(basePath + @"safeco\Life\LifeFromPreLife");
             safeco.Add(basePath + @"safeco\Life\LifeFromTrunk");
             safeco.Add(basePath + @"safeco\PreLife");
             safeco.Add(basePath + @"safeco\Test");
+            safeco.Add(basePath + @"safeco");
             folderContent.Add(basePath + @"safeco", safeco);
 
             List<string> shooger = new List<string>();
-            shooger.Add(basePath + @"shooger");
             shooger.Add(basePath + @"shooger\HotFix");
             shooger.Add(basePath + @"shooger\Life");
             shooger.Add(basePath + @"shooger\Life\LifeFromHotFix");
@@ -68,12 +73,13 @@ namespace ScriptProject
             shooger.Add(basePath + @"shooger\Life\LifeFromTrunk");
             shooger.Add(basePath + @"shooger\PreLife");
             shooger.Add(basePath + @"shooger\Test");
+            shooger.Add(basePath + @"shooger");
             folderContent.Add(basePath + @"shooger", shooger);
 
             List<string> SugarshackAnimation = new List<string>();
-            SugarshackAnimation.Add(basePath + @"SugarshackAnimation");
             SugarshackAnimation.Add(basePath + @"SugarshackAnimation\Life");
             SugarshackAnimation.Add(basePath + @"SugarshackAnimation\Test");
+            SugarshackAnimation.Add(basePath + @"SugarshackAnimation");
             folderContent.Add(basePath + @"SugarshackAnimation", SugarshackAnimation);
 
             List<string> Test = new List<string>();
@@ -81,7 +87,6 @@ namespace ScriptProject
             folderContent.Add(basePath + @"Test", Test);
 
             List<string> uncut = new List<string>();
-            uncut.Add(basePath + @"uncut");
             uncut.Add(basePath + @"uncut\Life");
             uncut.Add(basePath + @"uncut\Life\LifeFromPreLife");
             uncut.Add(basePath + @"uncut\Life\LifeFromStage");
@@ -89,10 +94,10 @@ namespace ScriptProject
             uncut.Add(basePath + @"uncut\PreLife");
             uncut.Add(basePath + @"uncut\Stage");
             uncut.Add(basePath + @"uncut\Test");
+            uncut.Add(basePath + @"uncut");
             folderContent.Add(basePath + @"uncut", uncut);
 
             List<string> usdirectory = new List<string>();
-            usdirectory.Add(basePath + @"usdirectory");
             usdirectory.Add(basePath + @"usdirectory\HotFix");
             usdirectory.Add(basePath + @"usdirectory\Life");
             usdirectory.Add(basePath + @"usdirectory\Life\LifeFromHotFix");
@@ -100,22 +105,14 @@ namespace ScriptProject
             usdirectory.Add(basePath + @"usdirectory\Life\LifeFromTrunk");
             usdirectory.Add(basePath + @"usdirectory\PreLife");
             usdirectory.Add(basePath + @"usdirectory\Test");
+            usdirectory.Add(basePath + @"usdirectory");
             folderContent.Add(basePath + @"usdirectory", usdirectory);
 
             return folderContent;
         }
-        //
-        
-        static void Main(string[] args)
+        //findign rows with script and parent paths in files
+        public static void successorRowSearcher(Dictionary<string, List<string>> folderContent)
         {
-
-            //da sa v otdelen klas ili inalicializirat
-            //da napravi i funkciite v novi klasove
-            //da probvam da go naprava s nova nahcin
-
-            //global path MUST BE CHANGE FOR OTHER DEVICES
-            string basePath = @"C:\Users\paco\Desktop\scripts\";
-
             //scriptPath key=path of file value=which file need to be added
             Dictionary<string, string> pathNames = new Dictionary<string, string>();
 
@@ -125,7 +122,6 @@ namespace ScriptProject
             //key=name of file, value=content of file
             Dictionary<string, string> pathValues = new Dictionary<string, string>();
 
-            Dictionary<string, List<string>> folderContent=Init(basePath);
             foreach (KeyValuePair<string, List<string>> file in folderContent)
             {
                 foreach (string path in file.Value)
@@ -192,19 +188,74 @@ namespace ScriptProject
                         }
                         catch { }
                     }
-                    //ScriptPath (working)
-                    try
-                    {
-                        foreach (KeyValuePair<string, string> item in pathNames)
-                        {
-                            string text = File.ReadAllText(item.Key);
-                            text = text.Replace("$scriptPath  = (Get-Item $PSScriptRoot).FullName", null);
-                            text = text.Replace(". \"$scriptPath\\" + item.Value, pathValues[item.Value]);
-                            string pathSave = System.IO.Path.Combine(path, item.Key.Substring(item.Key.LastIndexOf('\\') + 1));
-                            File.WriteAllText(pathSave, text);
-                        }
+                }
 
-                        //ScriptPath (working)
+                //ScriptPath + ParentPath replacement
+                try
+                {
+                    foreach (KeyValuePair<string, string> item in PpathNames)
+                    {
+                        string text = File.ReadAllText(item.Key);
+                        text = text.Replace("$parentPath  = (Get-Item $PSScriptRoot).Parent.FullName", null);
+                        text = text.Replace(". \"$parentPath\\" + item.Value, pathValues[item.Value]);
+                        File.WriteAllText(item.Key, text);
+                    }
+                    foreach (KeyValuePair<string, string> item in pathNames)
+                    {
+                        string text = File.ReadAllText(item.Key);
+                        text = text.Replace("$scriptPath  = (Get-Item $PSScriptRoot).FullName", null);
+                        text = text.Replace(". \"$scriptPath\\" + item.Value, pathValues[item.Value]);
+                        File.WriteAllText(item.Key, text);
+                    }
+                }
+                catch { }
+                //ScriptPath + ParentPath replacement
+
+                pathNames.Clear();
+                PpathNames.Clear();
+                pathValues.Clear();
+            }
+        }
+        // add new files for common virablees in every folder
+        public static void commonVariables(Dictionary<string, List<string>> folderContent)
+        {
+            Dictionary<string, int> keyValuePairs = new Dictionary<string, int>();
+            foreach (KeyValuePair<string, List<string>> file in folderContent)
+            {
+                foreach (string path in file.Value)
+                {
+                    //function to find which rows are variables
+                    int i = 0;
+                    foreach (string filepath in Directory.GetFiles(path, "*.ps1"))
+                    {
+                        i++;
+                        string[] readText = File.ReadAllLines(filepath);
+                        foreach (string s in readText)
+                        {
+                            if (s.Contains("$Global:") == true && (s[0] == '#' || s[0] == '$') && s[1] >= 33)
+                            {
+                                try
+                                {
+                                    keyValuePairs.Add(s, 1);
+                                }
+                                catch
+                                {
+                                    keyValuePairs[s]++;
+                                }
+                            }
+                        }
+                    }
+                    //adding variables used in every files in selected folder
+                    using (StreamWriter write = new StreamWriter(Path.Combine(path + "\\Variables.ps1")))
+                    {
+                        foreach (var item in keyValuePairs)
+                        {
+                            if (item.Value == i)
+                            {
+                                write.WriteLine(item.Key);
+                            }
+                        }
+                    }
 
                     keyValuePairs.Clear();
 
@@ -222,26 +273,47 @@ namespace ScriptProject
                             List<string> updatedLines = new List<string> { scriptPathLine, variablesLine };
                             updatedLines.AddRange(lines);
 
+                            File.WriteAllLines(filePath, updatedLines);
+                        }
+                        else
+                        {
+                            string[] lines = File.ReadAllLines(filePath).Where(line => !string.IsNullOrWhiteSpace(line) && !line.TrimStart().StartsWith("#")).ToArray();
+                            File.WriteAllLines(filePath, lines);
+                        }
+                    }
+                    // remove used viruables in other files
+                    foreach (string filePath in Directory.GetFiles(path, "*.ps1"))
+                    {
+                        if (filePath.Substring(path.Length) != "\\Variables.ps1")
+                        {
+                            string[] lines = File.ReadAllLines(Path.Combine(path + "\\Variables.ps1"));
+                            string[] linesToKeep = File.ReadLines(filePath).ToArray();
+                            for (int j = 0; j < lines.Length; j++)
+                            {
+                                linesToKeep = linesToKeep.Where(line => line != lines[j]).ToArray();
+                            }
+                            File.WriteAllLines(filePath, linesToKeep);
+                        }
+                    }
+                    //deleting empty variables.ps1 files
+                    foreach (string filePath in Directory.GetFiles(path, "Variables.ps1"))
+                    {
+                        if (new FileInfo(filePath).Length == 0)
+                        {
+                            File.Delete(filePath);
+                        }
+                    }
                 }
-                Console.WriteLine("---------------------");
-                foreach (KeyValuePair<string, string> item in PpathNames)
-                {
-                    Console.WriteLine(item);
-                }
-                Console.WriteLine();
-                foreach (KeyValuePair<string, string> item in pathNames)
-                {
-                    Console.WriteLine(item);
-                }
-                Console.WriteLine( );
-                foreach (var item in pathValues)
-                {
-                    Console.WriteLine(item.Key);
-                }
-                pathNames.Clear();
-                PpathNames.Clear();
-                pathValues.Clear();
             }
+        }
+        static void Main(string[] args)
+        {
+            //global path MUST BE CHANGE FOR OTHER DEVICES
+            string basePath = @"C:\Users\paco\Desktop\scripts\";
+
+            Dictionary<string, List<string>> folderContent = Init(basePath);
+            successorRowSearcher(folderContent);
+            commonVariables(folderContent);
         }
     }
 }
